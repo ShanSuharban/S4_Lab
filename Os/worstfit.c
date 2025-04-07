16365b11b656c2c32d7db211747f7fe7f54031f7
Muhammed Shan <95335433+ShanSuharban@users.noreply.github.com>
2025-04-07 08:11:58 +0530

    Update worstfit.c

--- Os/worstfit.c
+++ Os/worstfit.c
@@ -1,54 +1,53 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-    int no_of_process, no_of_blocks;
-    int block[10], process[10], allocation[10], blockNum[10];
-    int i, j, worstindex = -1;
+int main() {
+    int np, nb, b[10], p[10], a[10], blockNum[10];
+    int i, j, wi;
 
     printf("Enter the number of blocks: ");
-    scanf("%d", &no_of_blocks);
+    scanf("%d", &nb);
 
     printf("Enter the size of blocks: ");
-    for(i = 0; i < no_of_blocks; i++)
-        scanf("%d", &block[i]);
+    for(i = 0; i < nb; i++)
+        scanf("%d", &b[i]);
 
     printf("Enter the number of processes: ");
-    scanf("%d", &no_of_process);
+    scanf("%d", &np);
 
     printf("Enter the size of processes: ");
-    for(i = 0; i < no_of_process; i++) {
-        scanf("%d", &process[i]);
-        allocation[i] = -1;
+    for(i = 0; i < np; i++) {
+        scanf("%d", &p[i]);
+        a[i] = -1;
         blockNum[i] = -1;
     }
 
-    for(i = 0; i < no_of_process; i++) {
-        worstindex = -1;
-        for(j = 0; j < no_of_blocks; j++) {
-            if(block[j] >= process[i]) {
-                if(worstindex == -1 || block[j] > block[worstindex])
-                    worstindex = j;
+    for(i = 0; i < np; i++) {
+        wi = -1;
+        for(j = 0; j < nb; j++) {
+            if(b[j] >= p[i]) {
+                if(wi == -1 || b[j] > b[wi])
+                    wi = j;
             }
         }
 
-        if(worstindex != -1) {
-            allocation[i] = block[worstindex];
-            blockNum[i] = worstindex + 1;
-            block[worstindex] -= process[i];
+        if(wi != -1) {
+            a[i] = b[wi];
+            blockNum[i] = wi + 1;
+            b[wi] -= p[i];
         }
     }
 
     printf("After allocation:\n");
     printf("Process no\tProcess size\tBlock size\n");
-    for(i = 0; i < no_of_process; i++) {
-        if(allocation[i] != -1)
-            printf("%d\t\t%d\t\t%d\n", i + 1, process[i], allocation[i]);
+    for(i = 0; i < np; i++) {
+        if(a[i] != -1)
+            printf("%d\t\t%d\t\t%d\n", i + 1, p[i], a[i]);
         else
-            printf("%d\t\t%d\t\tCan't be allocated\n", i + 1, process[i]);
+            printf("%d\t\t%d\t\tCan't be allocated\n", i + 1, p[i]);
 
-        // printf("%d\t\t%d\t\tBlock %d\n", i + 1, process[i], blockNum[i]);
+        // printf("%d\t\t%d\t\tBlock %d\n", i + 1, p[i], blockNum[i]);
     }
 
     return 0;
 }
+
